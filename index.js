048bc57abd3247f260c9f4567632fb176ec55929
drcount-root <about.subhamsahu@gmail.com>
2022-09-16 11:38:16 +0530

    Second

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const add = (a, b) => a + b;
+const add = (...numbers) => numbers.reduce((x, y) => x + y);
 const sub = (a, b) => a - b;
-const mul = (a, b) => a * b;
+const mul = (...numbers) => numbers.reduce((x, y) => x * y);
 const div = (a, b) => a / b;
 
 export { add, sub, mul, div };
